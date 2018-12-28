e2f9b27b58b83847641fe866502973004e0f759e
Vincent Weevers <mail@vincentweevers.nl>
2018-12-28 11:34:09 +0100

    use stream.pipeline and destroy

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var exec = require('child_process').exec
   , path = require('path')
   , fs = require('fs')
   , PassThrough = require('readable-stream').PassThrough
+  , pipeline = require('readable-stream').pipeline
   , zlib = require('zlib')
   , tar = require('tar-stream')
 
@@ -13,22 +14,23 @@ module.exports = function canadianPub(root) {
   var out = new PassThrough();
 
   getMetadata(root, function(err, meta) {
-    if (err) return out.emit('error', err);
+    if (err) return out.destroy(err)
     out.emit('metadata', meta);
     listFiles(root, out);
-  });
+  })
+
   return out;
 }
 
 function getMetadata(root, callback) {
   exec('npm whoami', function (err, stdout, stderr) {
-    if (err) return callback('Cannot get current npm user');
+    if (err) return callback(err)
     var npmUser = stdout.trim();
     var packagePath = path.join(root, 'package.json');
     try {
       var pkg = require(packagePath);
     } catch (ex) {
-      return callback('error', 'Invalid package: ' + packagePath);
+      return callback(ex)
     }
     callback(null, {
       name: pkg.name,
@@ -40,27 +42,27 @@ function getMetadata(root, callback) {
 
 function listFiles(root, out) {
   exec('npm pack --ignore-scripts ' + root, function (err, stdout, stderr) {
-    if (err) return out.emit('error', 'Failed to pack archive: ' + err);
+    if (err) return out.destroy(err)
 
     // npm logs created filename on stdout
     var tarFile = path.join(process.cwd(), stdout.trim().split(/\n+/).pop());
 
-    fs.createReadStream(tarFile)
-      .on('error', out.emit.bind(out, 'error'))
-      .pipe(zlib.createGunzip())
-      .on('error', out.emit.bind(out, 'error'))
-      .pipe(tar.extract())
-      .on('error', out.emit.bind(out, 'error'))
-      .on('entry', function (header, stream, next) {
+    pipeline(
+      fs.createReadStream(tarFile),
+      zlib.createGunzip(),
+      tar.extract().on('entry', function (header, stream, next) {
         out.write(header.name.replace(/^package\//, '') + '\n');
         stream.resume()
         next()
-      })
-      .on('finish', function () {
+      }),
+      function (err) {
+        if (err) return out.destroy(err)
+
         fs.unlink(tarFile, function (err) {
-          if (err) return out.emit(err);
-          out.emit('end')
+          if (err) return out.destroy(err)
+          out.end()
         })
-      })
+      }
+    )
   })
 }
