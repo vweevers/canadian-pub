f7763cfc0969e1faa94531479b16b5a82da0882e
Vincent Weevers <mail@vincentweevers.nl>
2018-12-28 11:39:31 +0100

    switch from tap to tape

--- test/index.js
+++ test/index.js
@@ -1,23 +1,23 @@
 'use strict';
 /*jshint asi: true */
 
-var test = require('tap').test
+var test = require('tape')
   , canadianPub = require('../')
 
 function inspect(obj, depth) {
   console.error(require('util').inspect(obj, false, depth || 5, true));
 }
 
-test('\nfoo with .gitignore and no .npmignore', function (t) {
+test('foo with .gitignore and no .npmignore', function (t) {
   var root = __dirname + '/foo';
   var entries = []
   t.plan(4);
   canadianPub(root)
     .on('error', t.fail.bind(t))
     .on('metadata', function(meta) {
-      t.equal(meta.name, 'foo');
-      t.equal(meta.version, '1.2.3');
-      t.type(meta.user, 'string');
+      t.is(meta.name, 'foo');
+      t.is(meta.version, '1.2.3');
+      t.is(typeof meta.user, 'string');
     })
     .on('data', function (d) {
       entries.push(d.toString());
@@ -26,13 +26,11 @@ test('\nfoo with .gitignore and no .npmignore', function (t) {
       t.deepEqual(
           entries
         , [ 'package.json\n',
-            '.npmignore\n',
             'index.js\n',
             'example/first.js\n',
             'lib/work.js\n' ]
         , 'emits all files that would be published'
       )
-      t.end()
     });
 })
 
@@ -52,6 +50,5 @@ test('bar with prepublish script', function (t) {
             'index.js\n' ]
         , 'emits all files that would be published'
       )
-      t.end()
     });
 })
